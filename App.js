7d426d0a1a209249277787f75dfed428710f9cf5
zPopeyex <stven-457@hotmail.com>
2022-11-22 11:13:32 -0500

    Firebase implement

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
-import {View, Text, Button, StyleSheet, SafeAreaView} from 'react-native';
-import React, { useEffect, useState } from 'react';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  SafeAreaView,
+  Alert,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from './src/Pages/HomeScreen';
@@ -7,12 +14,12 @@ import ProfileScreen from './src/Pages/ProfileScreen';
 import ImportantPartsScreen from './src/Pages/ImportantPartsScreen';
 import NotationScreen from './src/Pages/NotationScreen';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import { auth } from './src/constants/firebase';
+import {auth} from './src/constants/firebase';
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
-  const [initializing, setInitializing] = useState(true);
+  const [initializing, setInitializing] = useState(false);
   const [user, setUser] = useState();
 
   // Handle user state changes
@@ -22,7 +29,14 @@ const App = () => {
   }
 
   useEffect(() => {
-    const subscriber = auth.onAuthStateChanged(onAuthStateChanged);
+    const subscriber = auth.onAuthStateChanged(
+      onAuthStateChanged(function (user, err) {
+        if (user) {
+          Actions.signUpVerify();
+        } else {
+        }
+      }),
+    );
     return subscriber; // unsubscribe on unmount
   }, []);
 
@@ -31,57 +45,25 @@ const App = () => {
   if (!user) {
     return (
       <SafeAreaView style={styles.container}>
-    <View>
-      <Text style={styles.title}>
-        The title and onPress handler are required. It is recommended to set accessibilityLabel to help make your app usable by everyone.
-      </Text>
-      <Button
-        title="Press me"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
-    </View>
-
-    <View>
-      <Text style={styles.title}>
-        Adjust the color in a way that looks standard on each platform. On  iOS, the color prop controls the color of the text. On Android, the color adjusts the background color of the button.
-      </Text>
-      <Button
-        title="Press me"
-        color="#f194ff"
-        onPress={() => Alert.alert('Button with adjusted color pressed')}
-      />
-    </View>
-
-    <View>
-      <Text style={styles.title}>
-        All interaction for the component are disabled.
-      </Text>
-      <Button
-        title="Press me"
-        disabled
-        onPress={() => Alert.alert('Cannot press this one')}
-      />
-    </View>
-
-    <View>
-      <Text style={styles.title}>
-        This layout strategy lets the title define the width of the button.
-      </Text>
-      <View style={styles.fixToText}>
-        <Button
-          title="Left button"
-          onPress={() => Alert.alert('Left button pressed')}
-        />
-        <Button
-          title="Right button"
-          onPress={() => Alert.alert('Right button pressed')}
-        />
-      </View>
-    </View>
-  </SafeAreaView>
+        <View>
+          <Text style={styles.title}>
+            The title and onPress handler are required. It is recommended to set
+            accessibilityLabel to help make your app usable by everyone.
+          </Text>
+          <Button
+            title="Press me"
+            onPress={async () => {
+              const res = await auth.createUserWithEmailAndPassword(
+                'stet@gmail.com',
+                '1234',
+              );
+              console.log(res);
+            }}
+          />
+        </View>
+      </SafeAreaView>
     );
   }
-  
 
   return (
     <NavigationContainer>
